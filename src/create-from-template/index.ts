69e5d7509fc079193cb8990f215520ee3249d011
Ryan Brennan <ryanbrennanbc1@gmail.com>
2020-06-06 18:19:32 -0700

    add: my-folder w/create-from-template

--- src/create-from-template/index.ts
+++ src/create-from-template/index.ts
@@ -1,10 +1,24 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
-
-
+import {
+  Rule,
+  SchematicContext,
+  Tree,
+  Source,
+  url,
+  mergeWith,
+  move,
+  apply,
+} from '@angular-devkit/schematics';
+import { normalize } from '@angular-devkit/core';
+//see options, think interface
+import { CreateFromTemplateOptions } from './schema';
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function createFromTemplate(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+export function createFromTemplate(options: CreateFromTemplateOptions): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    const source: Source = url('./files');
+    const transformedSource: Source = apply(source, [
+      move(normalize(options.folder)),
+    ]);
+    return mergeWith(transformedSource)(tree, context);
   };
 }
