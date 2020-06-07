f97799e1841d3a068d03ee5d273d4279b1c9de91
Ryan Brennan <ryanbrennanbc1@gmail.com>
2020-06-06 18:53:25 -0700

    add: dynamic content

--- src/create-from-template/index.ts
+++ src/create-from-template/index.ts
@@ -7,18 +7,25 @@ import {
   mergeWith,
   move,
   apply,
+  template,
 } from '@angular-devkit/schematics';
-import { normalize } from '@angular-devkit/core';
+import { normalize, strings } from '@angular-devkit/core';
 //see options, think interface
 import { CreateFromTemplateOptions } from './schema';
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function createFromTemplate(options: CreateFromTemplateOptions): Rule {
   return (tree: Tree, context: SchematicContext) => {
+    const folder = strings.dasherize(options.folder);
     const source: Source = url('./files');
     const transformedSource: Source = apply(source, [
-      move(normalize(options.folder)),
+      template({
+        filename: options.folder,
+        ...strings, // dasherize, classify, camelize, etc
+      }),
+      move(normalize(folder)),
     ]);
+
     return mergeWith(transformedSource)(tree, context);
   };
 }
